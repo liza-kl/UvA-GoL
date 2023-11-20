d85aec6c9d9903e16b14eb86ce216319a11d697d
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-11-20 14:26:05 +0100

    Generated first Model
    Created files automatically

---
+++ GoLLanguage/cps.gameoflife/src-gen/cps/gameoflife/parser/antlr/internal/InternalLsjatlParser.java
@@ -0,0 +1,316 @@
+package cps.gameoflife.parser.antlr.internal;
+
+import org.eclipse.xtext.*;
+import org.eclipse.xtext.parser.*;
+import org.eclipse.xtext.parser.impl.*;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.parser.antlr.AbstractInternalAntlrParser;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream;
+import org.eclipse.xtext.parser.antlr.XtextTokenStream.HiddenTokens;
+import org.eclipse.xtext.parser.antlr.AntlrDatatypeRuleToken;
+import cps.gameoflife.services.LsjatlGrammarAccess;
+
+
+
+import org.antlr.runtime.*;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+
+@SuppressWarnings("all")
+public class InternalLsjatlParser extends AbstractInternalAntlrParser {
+    public static final String[] tokenNames = new String[] {
+        "<invalid>", "<EOR>", "<DOWN>", "<UP>", "RULE_ID", "RULE_INT", "RULE_STRING", "RULE_ML_COMMENT", "RULE_SL_COMMENT", "RULE_WS", "RULE_ANY_OTHER", "'Hello'", "'!'"
+    };
+    public static final int RULE_ID=4;
+    public static final int RULE_WS=9;
+    public static final int RULE_STRING=6;
+    public static final int RULE_ANY_OTHER=10;
+    public static final int RULE_SL_COMMENT=8;
+    public static final int RULE_INT=5;
+    public static final int T__11=11;
+    public static final int RULE_ML_COMMENT=7;
+    public static final int T__12=12;
+    public static final int EOF=-1;
+
+    // delegates
+    // delegators
+
+
+        public InternalLsjatlParser(TokenStream input) {
+            this(input, new RecognizerSharedState());
+        }
+        public InternalLsjatlParser(TokenStream input, RecognizerSharedState state) {
+            super(input, state);
+             
+        }
+        
+
+    public String[] getTokenNames() { return InternalLsjatlParser.tokenNames; }
+    public String getGrammarFileName() { return "InternalLsjatl.g"; }
+
+
+
+     	private LsjatlGrammarAccess grammarAccess;
+
+        public InternalLsjatlParser(TokenStream input, LsjatlGrammarAccess grammarAccess) {
+            this(input);
+            this.grammarAccess = grammarAccess;
+            registerRules(grammarAccess.getGrammar());
+        }
+
+        @Override
+        protected String getFirstRuleName() {
+        	return "Model";
+       	}
+
+       	@Override
+       	protected LsjatlGrammarAccess getGrammarAccess() {
+       		return grammarAccess;
+       	}
+
+
+
+
+    // $ANTLR start "entryRuleModel"
+    // InternalLsjatl.g:64:1: entryRuleModel returns [EObject current=null] : iv_ruleModel= ruleModel EOF ;
+    public final EObject entryRuleModel() throws RecognitionException {
+        EObject current = null;
+
+        EObject iv_ruleModel = null;
+
+
+        try {
+            // InternalLsjatl.g:64:46: (iv_ruleModel= ruleModel EOF )
+            // InternalLsjatl.g:65:2: iv_ruleModel= ruleModel EOF
+            {
+             newCompositeNode(grammarAccess.getModelRule()); 
+            pushFollow(FOLLOW_1);
+            iv_ruleModel=ruleModel();
+
+            state._fsp--;
+
+             current =iv_ruleModel; 
+            match(input,EOF,FOLLOW_2); 
+
+            }
+
+        }
+
+            catch (RecognitionException re) {
+                recover(input,re);
+                appendSkippedTokens();
+            }
+        finally {
+        }
+        return current;
+    }
+    // $ANTLR end "entryRuleModel"
+
+
+    // $ANTLR start "ruleModel"
+    // InternalLsjatl.g:71:1: ruleModel returns [EObject current=null] : ( (lv_greetings_0_0= ruleGreeting ) )* ;
+    public final EObject ruleModel() throws RecognitionException {
+        EObject current = null;
+
+        EObject lv_greetings_0_0 = null;
+
+
+
+        	enterRule();
+
+        try {
+            // InternalLsjatl.g:77:2: ( ( (lv_greetings_0_0= ruleGreeting ) )* )
+            // InternalLsjatl.g:78:2: ( (lv_greetings_0_0= ruleGreeting ) )*
+            {
+            // InternalLsjatl.g:78:2: ( (lv_greetings_0_0= ruleGreeting ) )*
+            loop1:
+            do {
+                int alt1=2;
+                int LA1_0 = input.LA(1);
+
+                if ( (LA1_0==11) ) {
+                    alt1=1;
+                }
+
+
+                switch (alt1) {
+            	case 1 :
+            	    // InternalLsjatl.g:79:3: (lv_greetings_0_0= ruleGreeting )
+            	    {
+            	    // InternalLsjatl.g:79:3: (lv_greetings_0_0= ruleGreeting )
+            	    // InternalLsjatl.g:80:4: lv_greetings_0_0= ruleGreeting
+            	    {
+
+            	    				newCompositeNode(grammarAccess.getModelAccess().getGreetingsGreetingParserRuleCall_0());
+            	    			
+            	    pushFollow(FOLLOW_3);
+            	    lv_greetings_0_0=ruleGreeting();
+
+            	    state._fsp--;
+
+
+            	    				if (current==null) {
+            	    					current = createModelElementForParent(grammarAccess.getModelRule());
+            	    				}
+            	    				add(
+            	    					current,
+            	    					"greetings",
+            	    					lv_greetings_0_0,
+            	    					"cps.gameoflife.Lsjatl.Greeting");
+            	    				afterParserOrEnumRuleCall();
+            	    			
+
+            	    }
+
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop1;
+                }
+            } while (true);
+
+
+            }
+
+
+            	leaveRule();
+
+        }
+
+            catch (RecognitionException re) {
+                recover(input,re);
+                appendSkippedTokens();
+            }
+        finally {
+        }
+        return current;
+    }
+    // $ANTLR end "ruleModel"
+
+
+    // $ANTLR start "entryRuleGreeting"
+    // InternalLsjatl.g:100:1: entryRuleGreeting returns [EObject current=null] : iv_ruleGreeting= ruleGreeting EOF ;
+    public final EObject entryRuleGreeting() throws RecognitionException {
+        EObject current = null;
+
+        EObject iv_ruleGreeting = null;
+
+
+        try {
+            // InternalLsjatl.g:100:49: (iv_ruleGreeting= ruleGreeting EOF )
+            // InternalLsjatl.g:101:2: iv_ruleGreeting= ruleGreeting EOF
+            {
+             newCompositeNode(grammarAccess.getGreetingRule()); 
+            pushFollow(FOLLOW_1);
+            iv_ruleGreeting=ruleGreeting();
+
+            state._fsp--;
+
+             current =iv_ruleGreeting; 
+            match(input,EOF,FOLLOW_2); 
+
+            }
+
+        }
+
+            catch (RecognitionException re) {
+                recover(input,re);
+                appendSkippedTokens();
+            }
+        finally {
+        }
+        return current;
+    }
+    // $ANTLR end "entryRuleGreeting"
+
+
+    // $ANTLR start "ruleGreeting"
+    // InternalLsjatl.g:107:1: ruleGreeting returns [EObject current=null] : (otherlv_0= 'Hello' ( (lv_name_1_0= RULE_ID ) ) otherlv_2= '!' ) ;
+    public final EObject ruleGreeting() throws RecognitionException {
+        EObject current = null;
+
+        Token otherlv_0=null;
+        Token lv_name_1_0=null;
+        Token otherlv_2=null;
+
+
+        	enterRule();
+
+        try {
+            // InternalLsjatl.g:113:2: ( (otherlv_0= 'Hello' ( (lv_name_1_0= RULE_ID ) ) otherlv_2= '!' ) )
+            // InternalLsjatl.g:114:2: (otherlv_0= 'Hello' ( (lv_name_1_0= RULE_ID ) ) otherlv_2= '!' )
+            {
+            // InternalLsjatl.g:114:2: (otherlv_0= 'Hello' ( (lv_name_1_0= RULE_ID ) ) otherlv_2= '!' )
+            // InternalLsjatl.g:115:3: otherlv_0= 'Hello' ( (lv_name_1_0= RULE_ID ) ) otherlv_2= '!'
+            {
+            otherlv_0=(Token)match(input,11,FOLLOW_4); 
+
+            			newLeafNode(otherlv_0, grammarAccess.getGreetingAccess().getHelloKeyword_0());
+            		
+            // InternalLsjatl.g:119:3: ( (lv_name_1_0= RULE_ID ) )
+            // InternalLsjatl.g:120:4: (lv_name_1_0= RULE_ID )
+            {
+            // InternalLsjatl.g:120:4: (lv_name_1_0= RULE_ID )
+            // InternalLsjatl.g:121:5: lv_name_1_0= RULE_ID
+            {
+            lv_name_1_0=(Token)match(input,RULE_ID,FOLLOW_5); 
+
+            					newLeafNode(lv_name_1_0, grammarAccess.getGreetingAccess().getNameIDTerminalRuleCall_1_0());
+            				
+
+            					if (current==null) {
+            						current = createModelElement(grammarAccess.getGreetingRule());
+            					}
+            					setWithLastConsumed(
+            						current,
+            						"name",
+            						lv_name_1_0,
+            						"org.eclipse.xtext.common.Terminals.ID");
+            				
+
+            }
+
+
+            }
+
+            otherlv_2=(Token)match(input,12,FOLLOW_2); 
+
+            			newLeafNode(otherlv_2, grammarAccess.getGreetingAccess().getExclamationMarkKeyword_2());
+            		
+
+            }
+
+
+            }
+
+
+            	leaveRule();
+
+        }
+
+            catch (RecognitionException re) {
+                recover(input,re);
+                appendSkippedTokens();
+            }
+        finally {
+        }
+        return current;
+    }
+    // $ANTLR end "ruleGreeting"
+
+    // Delegated rules
+
+
+ 
+
+    public static final BitSet FOLLOW_1 = new BitSet(new long[]{0x0000000000000000L});
+    public static final BitSet FOLLOW_2 = new BitSet(new long[]{0x0000000000000002L});
+    public static final BitSet FOLLOW_3 = new BitSet(new long[]{0x0000000000000802L});
+    public static final BitSet FOLLOW_4 = new BitSet(new long[]{0x0000000000000010L});
+    public static final BitSet FOLLOW_5 = new BitSet(new long[]{0x0000000000001000L});
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-test
+# Game of Life
+
+- In order to produce the artifacts, right click on the Lsjatl.xtext
+and press "Generate as Xtext artifact"
+- In order to create instances of the *.lsjatl DSL right click on the cps.gameoflife 
+and "Launch Runtime Eclipse"
+- In the runtime: Create a new Project File -> New -> Project -> General -> Project
+- Name it, use deault location 
+- Finish, create new directory with cps.gameoflife as a parent
+- name directory specs 
+- Create new file. File name needs to have extenstion *.lsjatl (e.g. "test1.lsjatl") 
+- Project should be converted to xtext project 
